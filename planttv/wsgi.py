cd487358413a7d430d9b094f1513f4566619417f
Quentin Petraroia <q.petraroia@gmail.com>
2017-11-19 16:51:34 -0500

    fixed wsgi

--- planttv/wsgi.py
+++ planttv/wsgi.py
@@ -9,18 +9,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 import logging
-import os
-
-from dj_static import Cling
-
-from django.core.wsgi import get_wsgi_application
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planttv.settings")
 
 application = get_wsgi_application()
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "path.to.settings.py")
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(name)s %(levelname)-8s %(message)s")
