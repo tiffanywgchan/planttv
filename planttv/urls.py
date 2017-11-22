2c971336d097ffd1621f8d5e5f0d185302f389e8
Quentin Petraroia <q.petraroia@gmail.com>
2017-11-21 20:16:38 -0500

    added imports in the url.py

--- planttv/urls.py
+++ planttv/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include, url
 
 from planttv import views
 
